01c633e59baa2c76693316ede05fe15253ad4aa6
Suraj Kumar Yadav <105213644+suraj1494@users.noreply.github.com>
2024-02-24 11:04:10 +0530

    code committed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    {provide: LocationStrategy, useClass: HashLocationStrategy}, provideAnimationsAsync()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'userList',pathMatch:'full'},
+    {
+        path:'userList',
+        loadComponent : () => import('./user-list/user-list.component').then((mod)=>mod.UserListComponent)
+    }
+];
